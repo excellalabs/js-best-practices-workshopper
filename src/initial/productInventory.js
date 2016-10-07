20349a411b14c293667dfa8cfcf4e51a247a3a3e
Kevin Groat <kgroat09@gmail.com>
2016-10-06 20:51:35 -0400

    added inital code set

---
+++ src/initial/productInventory.js
@@ -0,0 +1,29 @@
+
+var products = [
+  {
+    name: 'Skittles',
+    price: 85,
+    id: 'A1'
+  }
+];
+
+module.exports = {
+  getProducts: function() { 
+    return [
+      {
+        name: 'Skittles',
+        price: 85,
+        id: 'A1'
+      }
+    ];
+  },
+  getProduct: function(productId) {
+    switch(productId){
+      case 'A1':
+        return {
+          name: 'Skittles',
+          price: 85
+        };
+    }
+  }
+};
