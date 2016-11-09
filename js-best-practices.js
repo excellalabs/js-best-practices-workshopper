701fbcc1d18ceeeb34b1e24ebb2f7f94126eb73f
Kevin Groat <apps@kevingroat.io>
2016-11-09 17:51:17 -0500

    Added custom commands

--- js-best-practices.js
+++ js-best-practices.js
@@ -4,7 +4,8 @@ const jsBestPractices = require('workshopper-adventure')({
     appDir: __dirname,
     languages: ['en'],
     header: require('workshopper-adventure/default/header'),
-    footer: require('workshopper-adventure/default/footer')
+    footer: require('workshopper-adventure/default/footer'),
+    commands: require('./helpers/customCommands')
 });
 
 jsBestPractices.addAll([
