799e2a709f1b9245122782914a5eac842e68c793
Kevin Groat <apps@kevingroat.io>
2016-11-09 21:08:42 -0500

    created quiz framework

--- helpers/customCommands.js
+++ helpers/customCommands.js
@@ -1,11 +1,17 @@
 
 var copyFiles = require('./commandHelpers/copyFiles');
+var quizRunner = require('./commandHelpers/quizRunner');
 
 module.exports = [{
   aliases: ['init'],
-  handler: function(scope) {
+  handler: function(shop) {
     var userDirectory = process.cwd();
     var initialDirectory = path.join(__dirname, '../src/initial');
     copyFiles(initialDirectory, userDirectory)
+  }
+}, {
+  aliases: ['first quiz'],
+  handler: function(shop) {
+    quizRunner(shop, 0).then(process.exit.bind(process, 0));
   }
 }];
