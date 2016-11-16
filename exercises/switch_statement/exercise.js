dd58b66ed8e9a9c5e998f24ff47c051dfa261f0c
samblissard <samblissard@gmail.com>
2016-11-16 18:11:48 -0500

    Finishing switch statement tests

--- exercises/switch_statement/exercise.js
+++ exercises/switch_statement/exercise.js
@@ -2,16 +2,37 @@ var exercise = require('workshopper-exercise')();
 var filecheck = require('workshopper-exercise/filecheck');
 var expect = require('chai').expect;
 var runFileStreamTests = require('../../helpers/runFileStreamTests');
+var runTests = require('../../helpers/runTests');
+
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise);
 
 // runs unit tests on submission
 exercise = runFileStreamTests(exercise, function(fileContents, test){
-  test('should not contain the keyword "switch"', function(){
+  test('changeHandler.js should not contain the keyword "switch"', function(){
     var regex = /switch/;
     expect(regex.test(fileContents)).to.equal(false);
   });
 });
 
+exercise = runTests(exercise, function(changeHandler, test) {
+  test('getAmount("p") should return 1', function() {
+    var expected = 1;
+    var actual = changeHandler.getAmount('p');
+    expect(actual).to.eql(expected);
+  });
+
+  test('getAmount("q") should return 25', function() {
+    var expected = 25;
+    var actual = changeHandler.getAmount('q');
+    expect(actual).to.eql(expected);
+  });
+
+  test('getAmount() should throw an error', function() {
+    expect(function(){
+      changeHandler.getAmount();
+    }).to.throw('Unrecognized coin undefined');
+  })
+});
 module.exports = exercise;
--- exercises/switch_statement/solution/solution.js
+++ exercises/switch_statement/solution/solution.js
@@ -15,17 +15,10 @@ var coins = {
 var coinsByAmount = ['q', 'd', 'n', 'p'];
 
 module.exports = {
-  convertToChange: function(amount) {
-    var change = [];
-    for(var i in coinsByAmount){
-      var coinType = coinsByAmount[i];
-      var coinValue = coins[coinType];
-
-      while(amount >= coinValue){
-        change.push(coinType);
-        amount -= coinValue;
-      }
+  getAmount: function(coinType) {
+    if(!coins.hasOwnProperty(coinType)){
+      throw new Error('Unrecognized coin ' + coinType);
     }
-    return change;
+    return coins[coinType];
   }
 };
